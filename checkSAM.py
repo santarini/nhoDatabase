1513323d150609835a892cee0fabe8627e7b22bb
MakoaWork <santarinimakoa@gmail.com>
2019-02-27 16:45:04 -1000

    Finshed Sam Checker. Did most of Sam Getter.

---
+++ checkSAM.py
@@ -0,0 +1,61 @@
+import os
+import requests
+import bs4 as bs
+import csv
+import re
+import time
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+#create webdriver
+#driver = webdriver.Chrome(r"C:\Users\CommandCenter\AppData\Local\Programs\Python\Python36-32\chromedriver.exe")
+driver = webdriver.Chrome(r"C:\Program Files\Python\Python36\chromedriver.exe")
+
+with open("test.csv") as csvfileA:
+    reader = csv.DictReader(csvfileA)
+    with open('Result.csv', 'a') as csvfileB:
+        fieldnames = ['8a', 'SAMResult']
+        writer = csv.DictWriter(csvfileB, fieldnames=fieldnames, lineterminator = '\n')
+        writer.writeheader()
+        for row in reader:
+            entryName = (row['8a'])
+            driver.get('https://www.sam.gov')
+            time.sleep(2)
+            driver.get('https://www.sam.gov/SAM/pages/public/searchRecords/search.jsf')
+            #get item by id='searchBasicForm:qterm_input'
+            content = driver.find_element_by_id('searchBasicForm:qterm_input')
+            #type value 8aNameEntry
+            #followed by ENTER
+            content.send_keys(entryName + '\n')
+            time.sleep(1)
+            try:
+                #get the text that is the h5 child of the the span class= results_body_text
+                CollegeName = driver.find_element_by_xpath('//*[@id="searchResultsID:dataTable"]/table[1]/tbody/tr/td/table/tbody/tr/td/li/table/tbody/tr/td/table[1]/tbody/tr/td[2]/span/h5')
+                writer.writerow({
+                    '8a': entryName,
+                    'SAMResult': CollegeName.text
+                    })
+            except NoSuchElementException:
+                writer.writerow({
+                                '8a': entryName,
+                                'SAMResult': "ERROR"
+                                })
+            clearButton = driver.find_element_by_xpath('//*[@id="samContentForm"]/table[2]/tbody/tr[2]/td/input')
+            clearButton.click()
+            time.sleep(1)
+            #driver.close()
+            #driver = webdriver.Chrome(r"C:\Program Files\Python\Python36\chromedriver.exe")
+
+#go to spreadsheet and get company names
+#seperate each name into a list
+
+#go to the SAM website
+#https://www.sam.gov/SAM/pages/public/searchRecords/search.jsf
+#Enter the company name
+#Hit enter/search
+#check to see if company is listed
+
+#STEP 2
+#click view details
---
+++ getSAM.py
@@ -0,0 +1,78 @@
+import os
+import requests
+import bs4 as bs
+import csv
+import re
+import time
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+#create webdriver
+#driver = webdriver.Chrome(r"C:\Users\CommandCenter\AppData\Local\Programs\Python\Python36-32\chromedriver.exe")
+driver = webdriver.Chrome(r"C:\Program Files\Python\Python36\chromedriver.exe")
+
+with open("samCheckResult.csv") as csvfileA:
+    reader = csv.DictReader(csvfileA)
+    with open('samGetResult.csv', 'a') as csvfileB:
+        fieldnames = ['SAMResult', 'NAICSCode', 'NAICSdesc']
+        writer = csv.DictWriter(csvfileB, fieldnames=fieldnames, lineterminator = '\n')
+        writer.writeheader()
+        for row in reader:
+            entryName = (row['SAMResult'])
+            driver.get('https://www.sam.gov')
+            time.sleep(2)
+            driver.get('https://www.sam.gov/SAM/pages/public/searchRecords/search.jsf')
+            #get item by id='searchBasicForm:qterm_input'
+            content = driver.find_element_by_id('searchBasicForm:qterm_input')
+            #type value 8aNameEntry
+            #followed by ENTER
+            content.send_keys(entryName + '\n')
+            time.sleep(1)
+            try:
+                #get the text that is the h5 child of the the span class= results_body_text
+                viewData = driver.find_element_by_xpath('//*[@id="searchResultsID:j_idt159:0:viewDetails"]')
+                viewData.click()
+                time.sleep(5)
+                assertionsButton = driver.find_element_by_id('entitySearchForm:entitySearchassertionReview')
+                assertionsButton.click()
+                time.sleep(3)
+                response = driver.page_source
+                soup = bs.BeautifulSoup(response, 'lxml')
+                table = soup.findAll('table')[2]
+                table = table.findAll('table')[4]
+                tbody = table.find('tbody')
+                for tr in table.findAll('tr')[1:]:
+                    if tr.findAll('td')[1].text == "Yes":
+                        NAICScode = tr.findAll('td')[0].text
+                        NAICSdesc = tr.findAll('td')[2].text
+                print(NAICScode)
+                writer.writerow({
+                    'SAMResult': entryName,
+                    'NAICSCode': NAICScode,
+                    'NAICSdesc': NAICSdesc,
+                    })
+            except NoSuchElementException:
+                writer.writerow({
+                    'SAMResult': entryName,
+                    'NAICSCode': "Not listed",
+                    'NAICSdesc': "Not listed",
+                                })
+            clearButton = driver.find_element_by_xpath('//*[@id="samContentForm"]/table[2]/tbody/tr[2]/td/input')
+            clearButton.click()
+            time.sleep(1)
+            #driver.close()
+            #driver = webdriver.Chrome(r"C:\Program Files\Python\Python36\chromedriver.exe")
+
+#go to spreadsheet and get company names
+#seperate each name into a list
+
+#go to the SAM website
+#https://www.sam.gov/SAM/pages/public/searchRecords/search.jsf
+#Enter the company name
+#Hit enter/search
+#check to see if company is listed
+
+#STEP 2
+#click view details
